cf7ab84b630c5af98e8b986f7e419749bbd90b19
Đoàn Trần Đức <doantranduc01@gmail.com>
2023-06-21 10:46:55 +0700

    Update AccountController.cs

--- DemoResfulAPI/Controllers/AccountController.cs
+++ DemoResfulAPI/Controllers/AccountController.cs
@@ -1,6 +1,22 @@
-﻿namespace DemoResfulAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Repository.Interfaces;
+
+namespace DemoResfulAPI.Controllers
 {
-    public class AccountController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
     {
+        private readonly IAccountRepository _accountRepository;
+        public AccountController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        [HttpGet]
+        public IActionResult Login(string username,string password) {
+            var account=_accountRepository.Login(username, password);
+            return Ok(account);
+        
+        }
     }
 }
